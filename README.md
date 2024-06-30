7019bd2389f530f014b00dd4f097c6784fffe9c2
xPBox <opensource@xpbox.org>
2024-06-30 21:28:00 +0800

    feat: text count

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
-# xPBox App Starter (Svelte + TS + Vite)
+# xPBox 字符统计工具 (Svelte + TS + Vite)
 
 [xPBox星盒应用平台](https://www.xpbox.cn/)是一个消费级aPaaS平台，本项目是xPBox应用开发示例
-
-## 使用
-
-```shell
-git clone https://github.com/xPBox/xpbox-svelte-vite.git
-cd xpbox-svelte-vite
-npm install
-```
--- src/App.svelte
+++ src/App.svelte
@@ -4,66 +4,96 @@
 
   const runtime = new Runtime()
 
-  let showJson = {}
-  let isReady = false
+  let text = ''
 
   onMount(() => {
-    const ready = () => {
-      if (!isReady) {
-        isReady = true
-        // 程序准备好了才显示，如果没有调用这个命令，页面上的组件不会被显示
-        runtime.app.ready()
+    runtime.app.ready()
+  })
+
+  const onClear = () => {
+    text = ""
+  }
+
+  let sTotal = 0; //双字节字符;
+  let iTotal = 0; //中文字符；
+  let eTotal = 0; //英文字符
+  let total = 0;
+  let iNum = 0;
+
+  $: {
+    let Words = text;
+    let W = {};
+    sTotal = 0;
+    iTotal = 0;
+    eTotal = 0;
+    total = 0;
+    iNum = 0;
+
+    let iNumWords = 0;
+    let sNumWords = 0;
+    let otherTotal = 0;
+    let bTotal = 0;
+
+    for (let i = 0; i < Words.length; i++) {
+      let c = Words.charAt(i);
+      if (c.match(/[\u4e00-\u9fa5]/)) {
+        if (isNaN(W[c])) {
+          iNumWords++;
+          W[c] = 1;
+        }
+        iTotal++;
       }
     }
 
-    // 获取配置信息
-    runtime.config.get().then(({ data, isReady }) => {
-      if (isReady) {
-        showJson = data
-        ready()
+    for (let i = 0; i < Words.length; i++) {
+      let c = Words.charAt(i);
+      if (c.match(/[^\x00-\xff]/)) {
+        if (isNaN(W[c])) {
+          sNumWords++;
+        }
+        sTotal++;
       }
-    })
-    // 应用加载的时候可能还没有拉到最新的配置，也就是说上面的get不一定能拉到真正的配置，
-    // 所以需要监听配置更新事件，当xPBox拉取到配置信息的时候会下发通知。这个通知不仅是程
-    // 序拉取到最新的配置，也可能是当前App或者其他App修改了配置也会触发更新通知。
-    runtime.config.onUpdate(({ data }) => {
-      showJson = data
-      ready()
-    })
-  })
-
-  const onSetExtraData = () => {
-    // 使用update只会更新提供的字段
-    runtime.config.update({ extra: 1 })
+      else {
+        eTotal++;
+      }
+      if (c.match(/[0-9]/)) {
+        iNum++;
+      }
+    }
+    total = iTotal * 2 + (sTotal - iTotal) * 2 + eTotal;
   }
-
-  const onSetRandomData = () => {
-    // 使用replace会替换掉整个配置
-    runtime.config.replace({ random: Math.random() })
-  }
-
-
 </script>
 
 <div class="main">
-  <div class="title">
-    Hello xPBox
+<!--  <div class="title">字符数统计</div>-->
+  <div class="editor-wrap">
+    <textarea class="editor" bind:value={text} />
+    <div class="footer">
+      <div style="flex: 1">
+        <div>
+          <span class="item">中文</span>
+          <span class="value">{iTotal}</span>
+          <span class="item">英文</span>
+          <span class="value">{eTotal}</span>
+        </div>
+        <div>
+          <span class="item">字符数</span>
+          <span class="value">{total}</span>
+          <span class="item">数字</span>
+          <span class="value">{iNum}</span>
+        </div>
+      </div>
+      <div class="btn" on:click={onClear}>清空</div>
+    </div>
   </div>
-
-  <div class="json">
-    {JSON.stringify(showJson, undefined, 4)}
-  </div>
-
-  <button class="btn" on:click={onSetRandomData}>
-    设置随机数据
-  </button>
-
-  <button class="btn" on:click={onSetExtraData}>
-    更新其他数据
-  </button>
 </div>
 
-<style>
+<style lang="scss">
+  .title {
+    color: #333;
+    margin-bottom: 12px;
+    font-size: 18px;
+  }
   .main {
     height: 100%;
     display: flex;
@@ -71,24 +101,63 @@
     justify-content: center;
     flex-direction: column;
     overflow: hidden;
+    box-sizing: border-box;
+    padding: 8px;
+    background: #fff;
   }
 
-  .title {
-    font-size: 26px;
-    font-weight: 500;
+  .editor {
+    outline: none;
+    margin-bottom: 12px;
     color: #333;
+    resize: none;
+    width: 100%;
+    height: 100%;
+    border: none;
+    padding: 12px 12px 56px;
   }
 
-  .json {
-    margin-top: 12px;
-    padding: 12px 24px;
+  .editor-wrap {
+    flex: 1;
+    width: 100%;
+    position: relative;
+    border: 1px solid #ccc;
     border-radius: 12px;
-    background: #cccccc80;
-    word-break: break-word;
-    white-space: pre-line;
+    overflow: hidden;
   }
 
-  .btn {
-    margin-top: 12px;
+  .footer {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    background: #eeeeeeaa;
+    align-items: center;
+    display: flex;
+    width: 100%;
+    padding: 6px 12px;
+    font-size: 12px;
+    box-sizing: border-box;
+    backdrop-filter: blur(4px);
+    border-radius: 0 0 12px 12px;
+
+    .value {
+      margin-left: 4px;
+      font-size: 14px;
+      color: #0080ee;
+      margin-right: 8px;
+    }
+
+    .btn {
+      border-radius: 4px;
+      background-color: #fff;
+      padding: 4px 8px;
+      user-select: none;
+      cursor: pointer;
+      transition: background-color 0.3s;
+
+      &:hover {
+        background-color: #eee;
+      }
+    }
   }
 </style>
